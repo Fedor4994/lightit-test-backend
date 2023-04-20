d573290c9adc3d3f5b908c5224a9dde683cd80ec
Fedor Sosnin <fedorsosnin04@gmail.com>
2023-04-20 17:28:06 +0300

    Make title as second-priority field for sorting

--- src/helpers/getSortType.ts
+++ src/helpers/getSortType.ts
@@ -1,23 +1,23 @@
 import { SortType } from "../types/sortType";
 
 export const getSortType = (sort: SortType) => {
-  let sortType: { [key: string]: 1 | -1 } = { rating: -1 };
+  let sortType: { [key: string]: 1 | -1 } = { rating: -1, title: 1 };
 
   switch (sort) {
     case "price-ask-rank":
-      sortType = { price: 1 };
+      sortType = { price: 1, title: 1 };
       break;
     case "price-desc-rank":
-      sortType = { price: -1 };
+      sortType = { price: -1, title: 1 };
       break;
     case "rating-ask-rank":
-      sortType = { rating: 1 };
+      sortType = { rating: 1, title: 1 };
       break;
     case "rating-desc-rank":
-      sortType = { rating: -1 };
+      sortType = { rating: -1, title: 1 };
       break;
     default:
-      sortType = { rating: -1 };
+      sortType = { rating: -1, title: 1 };
   }
 
   return sortType;
